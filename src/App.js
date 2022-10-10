8e9932b7ca033a6e90eda290cb5bcb4fddaa24c9
vikram-d1 <vikramd1001@gmail.com>
2022-10-10 11:31:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Howdy doing!!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
